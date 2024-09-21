8dab31382f9f7d7932dab66151bd43af040c8cd8
c08oprkiua <85906487+c08oprkiua@users.noreply.github.com>
2024-09-21 17:21:49 -0600

    Several animation changes
    * Added ice animations
    * Added skin/outfit switcher, switch by pressing tab
    * Tweaked the physics so that Sonic is slightly faster
    * Fixed walk animations, so that they use the main sprite sheet

---
+++ Demo/abilities/switch_skin.gd
@@ -0,0 +1,24 @@
+extends MoonCastAbility
+
+const switch_sfx_name:StringName = &"switch_character"
+
+@export var button_switch:StringName
+
+@export var skin_sfx:AudioStream
+
+@export var skins:Array[SpriteFrames]
+
+var current_skin:int = 0
+
+func _setup_2D(player:MoonCastPlayer2D) -> void:
+	player.add_edit_sound_effect(switch_sfx_name, skin_sfx)
+
+func _post_physics_2D(player:MoonCastPlayer2D) -> void:
+	if Input.is_action_just_pressed(button_switch):
+		current_skin += 1
+		if current_skin > skins.size() - 1:
+			current_skin = 0
+		
+		player.animated_sprite1.sprite_frames = skins[current_skin]
+		
+		player.play_sound_effect(switch_sfx_name)
