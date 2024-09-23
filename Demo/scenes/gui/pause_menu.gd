66a9b1a50fa4586422f7ecee6f3b3697617ce9fe
c08oprkiua <85906487+c08oprkiua@users.noreply.github.com>
2024-09-23 00:15:58 -0600

    Pause menu SFX, new GUI font

--- Demo/scenes/gui/pause_menu.gd
+++ Demo/scenes/gui/pause_menu.gd
@@ -2,8 +2,16 @@ extends CanvasLayer
 
 @export var pause_button:StringName
 
+@export var sfx_button_move:AudioStream
+
+@export var sfx_button_select:AudioStream
+
+@export var sfx_button_exit:AudioStream
+
 @onready var resume_button:Button = $"Panel/Margins/Sections/Main/menu/resume"
 @onready var ui_sections:TabContainer = $"Panel/Margins/Sections"
+
+@onready var audio:AudioStreamPlaybackPolyphonic
 
 var pause_active:bool = false
 
@@ -14,6 +22,8 @@ func _ready() -> void:
 	scene = get_tree()
 	custom_viewport = get_window().get_viewport()
 	$Panel/Margins/Sections/Main/Info/Version.text += ProjectSettings.get_setting("application/config/version")
+	$"AudioStreamPlayer".play()
+	audio = $"AudioStreamPlayer".get_stream_playback()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(pause_button):
@@ -26,18 +36,28 @@ func toggle_pause(paused:bool) -> void:
 	visible = paused
 	pause_active = paused
 	scene.paused = paused
+	if paused:
+		audio.play_stream(sfx_button_select)
+	else:
+		audio.play_stream(sfx_button_exit)
+
+func button_movement() -> void:
+	audio.play_stream(sfx_button_move)
 
 func _on_resume_pressed() -> void:
+	audio.play_stream(sfx_button_select)
 	if pause_active:
 		toggle_pause(false)
 
 func _on_restart_pressed() -> void:
 	toggle_pause(false)
+	audio.play_stream(sfx_button_select)
 	scene.reload_current_scene()
 
 func _on_options_pressed() -> void:
-	pass # Replace with function body.
+	audio.play_stream(sfx_button_select)
 
 func _on_quit_pressed() -> void:
+	audio.play_stream(sfx_button_exit)
 	OS.set_restart_on_exit(true)
 	scene.quit()
